830be6a664d78e6c1a9062ff7b20d6bec989b69b
Happy Machine <33182553+happy-machine@users.noreply.github.com>
2018-04-01 21:47:10 +0100

    Add files via upload

---
+++ safe-nestled-element.js
@@ -0,0 +1,19 @@
+function safe (object, path) {
+   
+    try {
+        var reg = new RegExp('(\\w+)\\[(\\d+)\\]');
+        return path.split('.').reduce(function (prev, curr) {
+            if (typeof prev != undefined && prev) {
+                if (reg.test(curr)) {
+                    return prev[reg.exec(curr)[1]][reg.exec(curr)[2]]
+                } else {
+                    return prev[curr] == undefined ? null : prev[curr]
+                }
+            } else {
+                return null
+            }
+        }, object || this)
+    } catch (e) {
+        return 'Error: Incorrect attributes in resolve function'
+    }
+}
